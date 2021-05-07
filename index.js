dfe21ab8a19840c713563b761bf105601f0ebadf
stkirk <shaanekirk@gmail.com>
2021-05-07 15:20:35 -0400

    task 2 complete, return array of the objects containing final stage data

--- index.js
+++ index.js
@@ -32,9 +32,13 @@ Use getFinals to do the following:
 hint - you should be looking at the stage key inside of the objects
 */
 
-function getFinals(/* code here */) {
-  /* code here */
+function getFinals(data) {
+  const finalsArray = data.filter(function (item) {
+    return item.Stage === "Final";
+  });
+  return finalsArray;
 }
+// console.log(getFinals(fifaData));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher-order function called getYears to do the following: 
