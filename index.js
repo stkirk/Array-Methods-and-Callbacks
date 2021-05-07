8523af6386c210c11241f3cb56461b4494d7fbbc
stkirk <shaanekirk@gmail.com>
2021-05-07 15:33:50 -0400

    completed Task 3, receive array and callback function to map the array created in the callback to display the years contained in the objects inside that array

--- index.js
+++ index.js
@@ -46,9 +46,13 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-  /* code here */
+function getYears(array, callback) {
+  const years = callback(array).map(function (item) {
+    return item.Year;
+  });
+  return years;
 }
+console.log(getYears(fifaData, getFinals));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher-order function getWinners to do the following:  
